f99cedd4eb2a12439573abac4a2b6c4829f6c9c8
ty-hag <hagen.tyler.stuff+1@gmail.com>
2020-12-29 14:18:38 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> oh boy
+          Edit <code>src/App.js</code> wow
         </p>
         <a
           className="App-link"
