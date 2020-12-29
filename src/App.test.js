85c30251f09ff861312c128df17dea85484612df
ty-hag <hagen.tyler.stuff+1@gmail.com>
2020-12-29 13:28:06 -0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('har har har hark', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
